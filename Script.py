b2e9c2dc56101c123f313406b51224b37471d0d1
Teen Fire <102863642+TeenOfFire@users.noreply.github.com>
2022-08-15 18:28:49 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -1,101 +1,88 @@
 class script(object):
     START_TXT = """𝙷𝙴𝙻𝙾 {},
-𝙼𝚈 𝙽𝙰𝙼𝙴 𝙸𝚂 <a href=https://t.me/{}>{}</a>, 𝙸 𝙲𝙰𝙽 𝙿𝚁𝙾𝚅𝙸𝙳𝙴 𝙼𝙾𝚅𝙸𝙴𝚂, 𝙹𝚄𝚂𝚃 𝙰𝙳𝙳 𝙼𝙴 𝚃𝙾 𝚈𝙾𝚄𝚁 𝙶𝚁𝙾𝚄𝙿 𝙰𝙽𝙳 𝙴𝙽𝙹𝙾𝚈 😍"""
+𝙼𝚈 𝙽𝙰𝙼𝙴 𝙸𝚂 <a href=https://t.me/{}>{}</a>, 𝙸 𝙲𝙰𝙽 𝙿𝚁𝙾𝚅𝙸𝙳𝙴 𝙰𝙽𝚈 𝙼𝙾𝚅𝙸𝙴, 𝚂𝙴𝚁𝙸𝙴𝚂, 𝙰𝙽𝙸𝙼𝙰𝚃𝙸𝙾𝙽 𝚎𝚝𝚌., 𝙴𝙽𝙹𝙾𝚈 🤩"""
     HELP_TXT = """𝙷𝙴𝚈 {}
 𝙷𝙴𝚁𝙴 𝙸𝚂 𝚃𝙷𝙴 𝙷𝙴𝙻𝙿 𝙵𝙾𝚁 𝙼𝚈 𝙲𝙾𝙼𝙼𝙰𝙽𝙳𝚂."""
     ABOUT_TXT = """✯ 𝙼𝚈 𝙽𝙰𝙼𝙴: {}
-✯ 𝙲𝚁𝙴𝙰𝚃𝙾𝚁: <a href=https://t.me/TeamEvamaria>Team Eva Maria</a>
-✯ 𝙻𝙸𝙱𝚁𝙰𝚁𝚈: 𝙿𝚈𝚁𝙾𝙶𝚁𝙰𝙼
-✯ 𝙻𝙰𝙽𝙶𝚄𝙰𝙶𝙴: 𝙿𝚈𝚃𝙷𝙾𝙽 𝟹
-✯ 𝙳𝙰𝚃𝙰 𝙱𝙰𝚂𝙴: 𝙼𝙾𝙽𝙶𝙾 𝙳𝙱
-✯ 𝙱𝙾𝚃 𝚂𝙴𝚁𝚅𝙴𝚁: 𝙷𝙴𝚁𝙾𝙺𝚄
-✯ 𝙱𝚄𝙸𝙻𝙳 𝚂𝚃𝙰𝚃𝚄𝚂: v1.0.1 [ 𝙱𝙴𝚃𝙰 ]"""
+✯ 𝙲𝚁𝙴𝙰𝚃𝙾𝚁: @TeenFire"""
     SOURCE_TXT = """<b>NOTE:</b>
-- Eva Maria is a open source project. 
-- Source - https://github.com/EvamariaTG/EvaMaria  
+- <b>Join & Get Any Movie or Series....</b>
+- <b>Group › https://t.me/HeroFlixGroup</b>
 
 <b>DEVS:</b>
-- <a href=https://t.me/TeamEvamaria>Team Eva Maria</a>"""
-    MANUELFILTER_TXT = """Help: <b>Filters</b>
+- <a href=https://t.me/HeroFlix>Team HeroFlix</a>"""
+    MANUELFILTER_TXT = """Help: <b>FILTERS »</b>
 
-- Filter is the feature were users can set automated replies for a particular keyword and EvaMaria will respond whenever a keyword is found the message
+» <b>Filter is A Feature Where Users can Set Automated Reply to a Specific Word</b>
 
-<b>NOTE:</b>
-1. eva maria should have admin privillage.
-2. only admins can add filters in a chat.
-3. alert buttons have a limit of 64 characters.
+<b>Important Notes:</b>
+1.<i> I Have To Be Admin </i>
+2.<i> Only admins can add Filters in Chat</i>
+3.<i> Buttons have a limit of 64 Characters</i>
 
 <b>Commands and Usage:</b>
-• /filter - <code>add a filter in chat</code>
-• /filters - <code>list all the filters of a chat</code>
-• /del - <code>delete a specific filter in chat</code>
-• /delall - <code>delete the whole filters in a chat (chat owner only)</code>"""
-    BUTTON_TXT = """Help: <b>Buttons</b>
+• <i> /filter - Add a Filter</i>
+• <i> /filters - List of All Filters</i>
+• <i> /del - Delete a Filter</i>
+• <i> /delall - Delete All Filters</i> """
+    BUTTON_TXT = """Help: <b>BUTTONS »</b>
 
-- Eva Maria Supports both url and alert inline buttons.
+» Supports both url and alert inline buttons.
 
 <b>NOTE:</b>
-1. Telegram will not allows you to send buttons without any content, so content is mandatory.
-2. Eva Maria supports buttons with any telegram media type.
-3. Buttons should be properly parsed as markdown format
+1. <i>Telegram will not allows you to send buttons without any content, so content is mandatory.</i>
+2. <i>Supports buttons with any telegram media type</i>
+3. <i>Buttons should be properly parsed as markdown format</i>
 
-<b>URL buttons:</b>
-<code>[Button Text](buttonurl:https://t.me/EvaMariaBot)</code>
+<b>URL Buttons:</b>
+<code>[Button Text](buttonurl:https://t.me/HeroFlixbot)</code>
 
-<b>Alert buttons:</b>
+<b>Alert Buttons:</b>
 <code>[Button Text](buttonalert:This is an alert message)</code>"""
-    AUTOFILTER_TXT = """Help: <b>Auto Filter</b>
+    AUTOFILTER_TXT = """Help: <b>AUTO FILTER »</b>
 
-<b>NOTE:</b>
-1. Make me the admin of your channel if it's private.
-2. make sure that your channel does not contains camrips, porn and fake files.
-3. Forward the last message to me with quotes.
- I'll add all the files in that channel to my db."""
+Add Me In Your Group as Admin & I Will Provide Any Movie, Series, Animation etc.,"""
     CONNECTION_TXT = """Help: <b>Connections</b>
 
 - Used to connect bot to PM for managing filters 
 - it helps to avoid spamming in groups.
 
 <b>NOTE:</b>
-1. Only admins can add a connection.
-2. Send <code>/connect</code> for connecting me to ur PM
+1. <i>Only Admins Can Add Connection</i>
+2. <i>Send <code>/connect</code> To Connect Me to Your PM</i>
 
 <b>Commands and Usage:</b>
-• /connect  - <code>connect a particular chat to your PM</code>
-• /disconnect  - <code>disconnect from a chat</code>
-• /connections - <code>list all your connections</code>"""
-    EXTRAMOD_TXT = """Help: <b>Extra Modules</b>
+•<i> /connect  - Connect a Chat to your PM</i>
+•<i> /disconnect  - Disconnect from a Chat</i>
+•<i> /connections - List Of All Connections</i>"""
+    EXTRAMOD_TXT = """Help: <b>Extra Features of Me »</b>
+
+<b>Commands and Usage:</b>
+•<i> /id - Get ID Of A User</i>
+•<i> /info  - Get Info About a User</i>
+•<i> /imdb  - Get Movie/Series Info from IMDb</i>"""
+    ADMIN_TXT = """Help: <b>ADMIN MODS »</b>
 
 <b>NOTE:</b>
-these are the extra features of Eva Maria
+This Works Only For Admins!
 
 <b>Commands and Usage:</b>
-• /id - <code>get id of a specified user.</code>
-• /info  - <code>get information about a user.</code>
-• /imdb  - <code>get the film information from IMDb source.</code>
-• /search  - <code>get the film information from various sources.</code>"""
-    ADMIN_TXT = """Help: <b>Admin mods</b>
+•<i> /stats - Get Status of DataBase</i>
+•<i> /delete - Delete A File</i>
+•<i> /users - List of My Users </i>
+•<i> /chats - Get List Of My Chats </i>
+•<i> /leave  - Leave from a chat</i>
+•<i> /disable  - Disable a Chat</i>
+•<i> /ban  - Ban a User</i>
+•<i> /unban  - Unban a User</i>
+•<i> /channel - List of All Connected Channels</i>
+•<i> /broadcast - Broadcast a Message to All Users</i>"""
+    STATUS_TXT = """★ 𝚃𝙾𝚃𝙰𝙻 𝙵𝙸𝙻𝙴𝚂: {}
+★ 𝚃𝙾𝚃𝙰𝙻 𝚄𝚂𝙴𝚁𝚂: {}
+★ 𝚃𝙾𝚃𝙰𝙻 𝙲𝙷𝙰𝚃𝚂: {}
+★ 𝚄𝚂𝙴𝙳 𝚂𝚃𝙾𝚁𝙰𝙶𝙴: {} 𝙼𝚒𝙱
+★ 𝙵𝚁𝙴𝙴 𝚂𝚃𝙾𝚁𝙰𝙶𝙴: {} 𝙼𝚒𝙱"""
 
-<b>NOTE:</b>
-This module only works for my admins
-
-<b>Commands and Usage:</b>
-• /logs - <code>to get the rescent errors</code>
-• /stats - <code>to get status of files in db.</code>
-• /delete - <code>to delete a specific file from db.</code>
-• /users - <code>to get list of my users and ids.</code>
-• /chats - <code>to get list of the my chats and ids </code>
-• /leave  - <code>to leave from a chat.</code>
-• /disable  -  <code>do disable a chat.</code>
-• /ban  - <code>to ban a user.</code>
-• /unban  - <code>to unban a user.</code>
-• /channel - <code>to get list of total connected channels</code>
-• /broadcast - <code>to broadcast a message to all users</code>"""
-    STATUS_TXT = """★ 𝚃𝙾𝚃𝙰𝙻 𝙵𝙸𝙻𝙴𝚂: <code>{}</code>
-★ 𝚃𝙾𝚃𝙰𝙻 𝚄𝚂𝙴𝚁𝚂: <code>{}</code>
-★ 𝚃𝙾𝚃𝙰𝙻 𝙲𝙷𝙰𝚃𝚂: <code>{}</code>
-★ 𝚄𝚂𝙴𝙳 𝚂𝚃𝙾𝚁𝙰𝙶𝙴: <code>{}</code> 𝙼𝚒𝙱
-★ 𝙵𝚁𝙴𝙴 𝚂𝚃𝙾𝚁𝙰𝙶𝙴: <code>{}</code> 𝙼𝚒𝙱"""
     LOG_TEXT_G = """#NewGroup
 Group = {}(<code>{}</code>)
 Total Members = <code>{}</code>

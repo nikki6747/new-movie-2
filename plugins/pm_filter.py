461f7689c0ed322039390fcf4fd1df3f9ee7c0a0
Teen Fire <102863642+TeenOfFire@users.noreply.github.com>
2022-08-15 19:23:30 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -764,7 +764,7 @@ async def advantage_spell_chok(msg):
     movielist += [(re.sub(r'(\-|\(|\)|_)', '', i, flags=re.IGNORECASE)).strip() for i in gs_parsed]
     movielist = list(dict.fromkeys(movielist))  # removing duplicates
     if not movielist:
-        k = await msg.reply("I couldn't find anything related to that. Check your spelling")
+        k = await msg.reply(" <b><i>💥Please Select Correct Name 👇</i></b> \n <i><b>💥कृपया सही नाम चुनें 👇</i></b> ")
         await asyncio.sleep(8)
         await k.delete()
         return
@@ -776,7 +776,7 @@ async def advantage_spell_chok(msg):
         )
     ] for k, movie in enumerate(movielist)]
     btn.append([InlineKeyboardButton(text="Close", callback_data=f'spolling#{user}#close_spellcheck')])
-    await msg.reply("I couldn't find anything related to that\nDid you mean any one of these?",
+    await msg.reply(" <b><i>💥Please Select Correct Name 👇</i></b> \n <i><b>💥कृपया सही नाम चुनें 👇</i></b> ",
                     reply_markup=InlineKeyboardMarkup(btn))
 
 
